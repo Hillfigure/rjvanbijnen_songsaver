23eb93ebb0d4d5166d568bf6141765e44fd69e06
Hillfigure <49096115+Hillfigure@users.noreply.github.com>
2022-01-21 19:29:35 +0100

    Delete src/reducers directory

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-// import reducers here
-import { combineReducers } from 'redux';
-
-const allReducers = combineReducers({
-    // counter: counterReducer,
-    // isLogged: loggedReducer
-})
-
-export default allReducers
